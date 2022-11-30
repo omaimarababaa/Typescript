266984a6873e0e39e93baaa5ffd4a48012af5230
omaimarababaa <omaima.rababaa@gmail.com>
2022-11-30 19:59:16 +0300

    solve problem

---
+++ src/05-assigning-types-to-variables.problem.ts
@@ -0,0 +1,28 @@
+import { expect, it } from "vitest";
+import { string } from "zod";
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  isAdmin: boolean;
+}
+
+/**
+ * How do we ensure that defaultUser is of type User
+ * at THIS LINE - not further down in the code?
+ */
+const defaultUser:User= {
+  id: 1,
+  firstName: "omaima",
+  lastName: "rababaa",
+  isAdmin: true,
+};
+
+const getUserId = (user: User) => {
+  return user.id;
+};
+
+it("Should get the user id", () => {
+  expect(getUserId(defaultUser)).toEqual(1);
+});
---
+++ src/14-extends.problem.ts
@@ -0,0 +1,33 @@
+import { Equal, Expect } from "./helpers/type-utils";
+
+/**
+ * Here, the id property is shared between all three
+ * interfaces. Can you find a way to refactor this to
+ * make it more DRY?
+ */
+interface Boss{
+  id: string;
+}
+
+interface User extends Boss {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface Post extends Boss{
+  id: string;
+  title: string;
+  body: string;
+}
+
+interface Comment extends Boss{
+  id: string;
+  comment: string;
+}
+
+type tests = [
+  Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
+  Expect<Equal<Post, { id: string; title: string; body: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>,
+];
